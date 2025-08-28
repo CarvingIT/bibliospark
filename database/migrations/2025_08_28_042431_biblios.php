05ea1f1750d28b4424ed98c7a90f06a0b836bc3f
Ketan Kulkarni <ketan@carvingit.com>
2025-08-28 10:32:58 +0530

    created db tables and models

---
+++ database/migrations/2025_08_28_042431_biblios.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+            Schema::create('biblios', function (Blueprint $table) {
+                $table->id(); 
+                $table->string('external_id')->unique();
+                $table->string('isbn_10', 10)->nullable()->unique();
+                $table->string('isbn_13', 13)->nullable()->unique();
+                $table->string('issn', 8)->nullable()->unique();
+                $table->string('title');
+                $table->text('authors')->nullable();
+                $table->text('publishers')->nullable();
+                $table->string('edition')->nullable();
+                $table->text('cover_images')->nullable();
+                $table->text('all_details');
+                $table->timestamps(); 
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('biblios');
+    }
+};
